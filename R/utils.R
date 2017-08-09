c7df50eaa5f4ff2668369e1fba21eac0697697bd
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-08-09 19:28:51 +0200

    got loops from boxjson

--- R/utils.R
+++ R/utils.R
@@ -112,23 +112,25 @@ mutateInputJSON <- function(json) {
 #' @return character. Vector of splits.
 #'
 #' @keywords internal
-splitOnUnclosedChar <- function(string, char, keep=FALSE) {
-  stopifnot(isTruthyChr(string), isTruthyChr(char), nchar(char) == 1L,
+splitOnUnclosedChar <- function(string, peep, keep=FALSE) {
+  stopifnot(isTruthyChr(string), isTruthyChr(peep), nchar(peep) == 1L,
             is.logical(keep))
   # split to single characters
   chars <- strsplit(string, '', fixed=TRUE)[[1L]]
   # setup
+  accu <- vector('character')
+  BRACKETS <- list(OPEN=c('[', '{'), CLOSE=c(']', '}'))
   opbr <- 0L      # if opbr is zero we r not in a struct
   qtct <- 0L      # if even we r not in a string
   last.cut <- 0L  # tracks last slice index
-  accu <- vector('character')
+  i <- 1L
   prev <- chars[1L]
-  # peep through --- NEED 2 MAKE THIS ONE WORK LIKE THE OTHER !!!
-  for (i in seq_along(chars)) {
-    if (chars[i] %in% c('[', '{')) opbr <- opbr + 1L
-    if (chars[i] %in% c(']', '}')) opbr <- opbr - 1L
-    if (chars[i] == '"' && prev != '\\') qtct <- qtct + 1L
-    if (chars[i] == char && (opbr == 0L && qtct %% 2L == 0L)) {
+  # peep through
+  for (char in chars) {
+    if (char %in% BRACKETS$OPEN) opbr <- opbr + 1L
+    if (char %in% BRACKETS$CLOSE) opbr <- opbr - 1L
+    if (char == '"' && prev != '\\') qtct <- qtct + 1L
+    if (char == peep && (opbr == 0L && qtct %% 2L == 0L)) {
       if (!keep) {
         accu <- append(accu, substr(string, last.cut + 1L, i - 1L))
       } else {  # keep split character
@@ -140,7 +142,8 @@ splitOnUnclosedChar <- function(string, char, keep=FALSE) {
       }
       last.cut <- i
     }
-    prev <- chars[i]
+    i <- i + 1L
+    prev <- char
   }
   # consume remainder
   if (last.cut < nchar(string))  {
@@ -436,23 +439,24 @@ extractValueFromObjKey <- function(obj, key) {
 #' @return Logical.
 #'
 #' @keywords internal
-hasUnclosedChar <- function(string, char) {
-  stopifnot(isTruthyChr(string), isTruthyChr(char), nchar(char) == 1L)
+hasUnclosedChar <- function(string, peep) {
+  stopifnot(isTruthyChr(string), isTruthyChr(peep), nchar(peep) == 1L)
   # split to single characters
   chars <- strsplit(string, '')[[1L]]
   # setup
+  BRACKETS <- list(OPEN=c('[', '{'), CLOSE=c(']', '}'))
   opbr <- 0L     # if opbr is zero we r not in a struct
   qtct <- 0L     # if even we r not in a string
   prev <- chars[1L]
   # peep through --- THIS VERSION IS COOL
-  for (i in seq_along(chars)) {
-    if (chars[i] %in% c('[', '{')) opbr <- opbr + 1L
-    if (chars[i] %in% c(']', '}')) opbr <- opbr - 1L
-    if (chars[i] == '"' && prev != '\\') qtct <- qtct + 1L
-    if (chars[i] == char && (opbr == 0L && qtct %% 2L == 0L)) {
+  for (char in chars) {
+    if (char %in% BRACKETS$OPEN) opbr <- opbr + 1L
+    if (char %in% BRACKETS$CLOSE) opbr <- opbr - 1L
+    if (char == '"' && prev != '\\') qtct <- qtct + 1L
+    if (char == peep && (opbr == 0L && qtct %% 2L == 0L)) {
       return(TRUE)
     }
-    prev <- chars[i]
+    prev <- char
   }
   return(FALSE)
 }
